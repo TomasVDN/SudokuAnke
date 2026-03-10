adfc644c29b7a987b57168521cbc0f1f07cb8b53
tomas <tomas.vdn@hotmail.com>
2026-03-10 21:36:00 +0100

    Bump minimum sdk to 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sudokuanke"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
